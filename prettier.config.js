5db77476d266db404cf7e180ea49afbf6df18911
kress95 <cferraz95@gmail.com>
2019-01-04 19:51:50 -0300

    First commit

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: "typescript",
+  printWidth: 80,
+  proseWrap: "always",
+  trailingComma: "all",
+  bracketSpacing: false,
+  arrowParens: "avoid",
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+};
+
